e1246ace49ea1d34787ce163ae82af9c139dfb41
Julien <julien@ripouteau.com>
2023-07-02 20:37:55 +0200

    perf: improve add-js-extensions

--- src/patchers/add_js_extensions/index.ts
+++ src/patchers/add_js_extensions/index.ts
@@ -1,3 +1,4 @@
+import { ImportDeclaration } from 'ts-morph'
 import { PatcherFactory } from '../../types/index.js'
 import { BasePatcher } from '../base_patcher.js'
 
@@ -15,11 +16,19 @@ export function addJsExtensions(): PatcherFactory {
 export class AddJsExtensions extends BasePatcher {
   static patcherName = 'add-js-extensions'
 
+  #changes: {
+    importNode: ImportDeclaration
+    newModuleSpecifier: string
+  }[] = []
+
   invoke(): void {
     super.invoke()
 
     const tsFiles = this.project.getSourceFiles()
 
+    /**
+     * Keep track of all the changes and then apply them
+     */
     for (let tsFile of tsFiles) {
       const imports = tsFile.getImportDeclarations()
 
@@ -36,12 +45,20 @@ export class AddJsExtensions extends BasePatcher {
         const importedSourcePath = importNode.getModuleSpecifierSourceFile()?.getFilePath()
 
         if (importedSourcePath?.endsWith('index.ts') && !moduleSpecifier.endsWith('index.js')) {
-          importNode.setModuleSpecifier(`${moduleSpecifier}/index.js`)
+          this.#changes.push({ importNode, newModuleSpecifier: `${moduleSpecifier}/index.js` })
           continue
         }
 
-        importNode.setModuleSpecifier(`${moduleSpecifier}.js`)
+        this.#changes.push({ importNode, newModuleSpecifier: `${moduleSpecifier}.js` })
       }
+    }
+
+    /**
+     * Apply all changes at once. This is important to keep
+     * good performances
+     */
+    for (let change of this.#changes) {
+      change.importNode.setModuleSpecifier(change.newModuleSpecifier)
     }
 
     this.exit()
